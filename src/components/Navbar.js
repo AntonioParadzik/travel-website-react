685008cf45c6944c9b1b1ddfb68fabc9434e5fd8
AntonioParadzik <antonioparadzik1@gmail.com>
2024-01-02 16:13:41 +0100

    Add dynamic navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import { Component } from "react";
+import "./NavbarStyles.css";
+import { MenuItems } from "./MenuItems"
+
+class Navbar extends Component{
+    render(){
+        return(
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">Trippy</h1>
+
+                <ul className="nav-menu">
+                    {MenuItems.map((item, index) => {
+                        return(
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                    <i className={item.icon}></i>
+                                    {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                    <button>Sign Up</button>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
