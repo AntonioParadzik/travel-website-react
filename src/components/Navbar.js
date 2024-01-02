1e42aa5be9f41b0ff5f2850d518b8f37fb4f2985
AntonioParadzik <antonioparadzik1@gmail.com>
2024-01-02 18:25:27 +0100

    Add responsiveness to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,20 @@ import "./NavbarStyles.css";
 import { MenuItems } from "./MenuItems"
 
 class Navbar extends Component{
+    state = {clicked: false};
+    handleClick = () =>{
+        this.setState({clicked: !this.state.clicked})
+    }
     render(){
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">Trippy</h1>
 
-                <ul className="nav-menu">
+                <div className="menu-icons" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                </div>
+
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
                     {MenuItems.map((item, index) => {
                         return(
                             <li key={index}>
